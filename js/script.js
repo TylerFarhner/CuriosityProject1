c2445a25fa59b14b52857c41849f8487d06f67a5
Tyler Farhner <tylerfarhner@gmail.com>
2020-10-25 21:35:12 -0700

    first commit:

---
+++ js/script.js
@@ -0,0 +1,3 @@
+let sheetURL = "https://docs.google.com/spreadsheets/d/1Wvq29dYTUKc9L_ZJxz-btf7KFa3FGp1WdUMONeYaFYQ/edit?usp=sharing"
+
+let sheetAsJSON = "https://spreadsheets.google.com/feeds/list/1Wvq29dYTUKc9L_ZJxz-btf7KFa3FGp1WdUMONeYaFYQ/od6/public/values?alt=json"
